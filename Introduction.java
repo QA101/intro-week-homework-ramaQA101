3aaa06e0a1b3667e7107c25555d935292273a2ae
rama <rowee09@icloud.com>
2017-10-09 20:06:46 -0700

    homework submit

---
+++ Introduction.java
@@ -0,0 +1,19 @@
+
+public class Introduction {
+	
+	public static void main(String []args )
+	{
+		System.out.println("Hello! My Name is Rama Kelkar");
+		System.out.println("I ive in Redmond,Washington");
+		System.out.println("I moved to to USA in 2012");
+		System.out.println("I am IT Engineer");
+		System.out.println("I have learnt a little JAVA before");
+		System.out.println("I like this course");
+		System.out.println("I think  this is going to be more challenging");
+		System.out.println("I need to practice everyday");
+		System.out.println("Using git is challenging");
+		System.out.println("I am going to try my best!!");
+	
+		
+	}
+}
